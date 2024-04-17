d834d05437d93e85e6a93bd5a236cecbb60d2017
Rumbleminze <rumbleminze@mybuddywilson.com>
2024-04-16 19:08:30 -0500

    adding build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+export PATH=$PATH:../cc65-snapshot-win32/bin
+export GAME=SETGAMENAME
+set -e
+
+cd "$(dirname "$0")"
+
+mkdir -p out
+ca65 ./src/main.asm -o ./out/main.o -g
+ld65 -C ./src/hirom.cfg -o ./out/$(GAME).sfc ./out/main.o
